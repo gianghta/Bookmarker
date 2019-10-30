ce106f2dbd546d87fb14e90aa61fa30bb73a5ee0
gianghta <thoanggiang98@gmail.com>
2019-10-29 22:49:35 -0300

    Create add button

---
+++ js/popup.js
@@ -0,0 +1,8 @@
+const addBtn = document.querySelector('#addBtn');
+const clearAllBtn = document.querySelector('#clearBtn');
+
+console.log('Loaded resource');
+
+addBtn.addEventListener('click', () => {
+    console.log('Button clicked');
+});
