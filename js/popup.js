1779e4abae03e026202073fe8823733d82e06ec9
gianghta <thoanggiang98@gmail.com>
2019-10-31 22:27:03 -0300

    Add test function to retrieve tab url

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,21 @@
 const addBtn = document.querySelector('#addBtn');
 const clearAllBtn = document.querySelector('#clearBtn');
+const ul = document.querySelector('.list-unstyled');
 
 console.log('Loaded resource');
 
 addBtn.addEventListener('click', () => {
-    console.log('Button clicked');
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs) {
+        let url = tabs[0].url;
+        let title = tabs[0].title;
+        let icon = tabs[0].favIconUrl;
+        console.log('Current tabs:', url);
+        console.log('Title: ', title);
+        console.log('Icon: ', icon);
+    });
 });
+
+clearAllBtn.addEventListener('click', () => {
+    console.log('Clearing list...');
+});
+
