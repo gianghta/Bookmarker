56b9b194e8552d5323464bcc8134096c60fa7c7d
gianghta <thoanggiang98@gmail.com>
2019-11-02 16:00:43 -0300

    Create add/remove link functions on UI

--- src/popup.js
+++ src/popup.js
@@ -4,31 +4,63 @@ const ul = document.querySelector('.list-unstyled');
 
 console.log('Loaded resource');
 
-function createHtmlLink(tabs) {
+function addLink(tabs) {
     let url = tabs[0].url;
-    let title = tabs[0].title;
-    let icon = tabs[0].favIconUrl;
-    console.log('Current tabs:', url);
-    console.log('Title: ', title);
-    console.log('Icon: ', icon);
+    let title = ` ${tabs[0].title}`;
+    let iconUrl = tabs[0].favIconUrl;
 
+    // List item
     const li = document.createElement('li');
-    const a = document.createElement('a');
-    a.setAttribute('href', '');
-    a.setAttribute('title', '');
-    a.textContent = title;
-    const span = document.createElement('span');
-    span.classList.add('float-right');
-    span.textContent = 13;
+    li.className = 'd-flex justify-content-between align-items-center';
 
-    a.appendChild(span);
+    // Favicon
+    const icon = document.createElement('img');
+    icon.setAttribute('src', iconUrl);
+
+    // Title
+    const a = document.createElement('a');
+    a.setAttribute('href', url);
+    a.setAttribute('target', '_blank');
+    const titleNode = document.createTextNode(title);
+
+    // Delete button
+    const deleteBtn = document.createElement('a');
+    deleteBtn.className = 'delete-item float-right fa fa-times';
+
+    // Setting up and adding link to the list
+    a.appendChild(icon);
+    a.appendChild(titleNode);
     li.appendChild(a);
+    li.appendChild(deleteBtn);
     ul.appendChild(li);
 }
 
+function removeLink(e) {
+    if (e.target.classList.contains('delete-item')) {
+        if (confirm('Are you sure?')) {
+            e.target.parentElement.remove();
+        }
+    }
+    console.log('Link removed');
+}
+
+function addLinkToStorage() {
+    
+}
+
+function removeLinkFromStorage() {
+
+}
+
+function loadLinkFromStorage() {
+
+}
+
+ul.addEventListener('click', removeLink);
+
 addBtn.addEventListener('click', () => {
     chrome.tabs.query({'active': true, 'currentWindow': true}, function(tabs) {
-        createHtmlLink(tabs);
+        addLink(tabs);
     });
 });
 
